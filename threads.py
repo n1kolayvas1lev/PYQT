b2f536841f1c447fa55767148cc46942f80d778e
n1kolayvas1lev <ckb01@ya.ru>
2022-06-25 10:57:07 +0300

    Практика 3 init Thread

--- threads.py
+++ threads.py
@@ -6,22 +6,57 @@ class MyApp(QtWidgets.QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.initUi()
+        self.initThreads()
+
+    def initThreads(self):
+        self.timerThread = TimerThread()
 
     def initUi(self):
-        self.lineEdirStart = QtWidgets.QLineEdit()
-        self.lineEdirStart.setPlaceholderText('Введите количество секунд.')
+        # ui
+        self.setFixedSize(180, 150)  # Установка фиксированных размеров окна
+
+        self.lineEditStart = QtWidgets.QLineEdit()
+        self.lineEditStart.setPlaceholderText('Введите количество секунд.')
 
         self.pushButtonStart = QtWidgets.QPushButton()
         self.pushButtonStart.setText('Start')
+
         self.pushButtonStop = QtWidgets.QPushButton()
         self.pushButtonStop.setText('Stop')
 
+        # Изменение политик растяжения кнопок.
+        # self.pushButtonStop.setSizePolicy(QtWidgets.QSizePolicy.Policy.Expanding,
+        #                                    QtWidgets.QSizePolicy.Policy.Expanding)
+        # self.pushButtonStart.setSizePolicy(QtWidgets.QSizePolicy.Policy.Expanding,
+        #                                    QtWidgets.QSizePolicy.Policy.Expanding)
+
         layout = QtWidgets.QVBoxLayout()
-        layout.addWidget(self.lineEdirStart)
+        layout.addWidget(self.lineEditStart)
         layout.addWidget(self.pushButtonStart)
         layout.addWidget(self.pushButtonStop)
         self.setLayout(layout)
+        # Добавление спейсера на форму для занятия всего свободного пространства. 1 - размер в пикс.
+        # layout.addSpacerItem(
+        #     QtWidgets.QSpacerItem(
+        #         1,
+        #         1,
+        #         QtWidgets.QSizePolicy.Policy.Expanding,
+        #         QtWidgets.QSizePolicy.Policy.Expanding
+        #     )
+        # )
 
+        # widgets signals
+        self.pushButtonStart.clicked.connect(self.onPushButtonStartClicked)
+
+    def onPushButtonStartClicked(self):
+        self.timerThread.start()
+
+class TimerThread(QtCore.QThread):
+
+    def run(self) -> None:
+        for i in range(10, 0, -1):
+            print(i)
+            time.sleep(1)
 
 
 if __name__ == '__main__':
